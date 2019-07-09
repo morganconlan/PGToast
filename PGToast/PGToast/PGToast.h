addf1e53d9c527614c529fb370c9d69401386d07
Morgan Conlan <morgan@pangr.ie>
2019-07-09 20:22:45 +0100

    Create framework

---
+++ PGToast/PGToast/PGToast.h
@@ -0,0 +1,19 @@
+//
+//  PGToast.h
+//  PGToast
+//
+//  Created by Morgan Conlan on 09/07/2019.
+//  Copyright © 2019 Morgan Conlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGToast.
+FOUNDATION_EXPORT double PGToastVersionNumber;
+
+//! Project version string for PGToast.
+FOUNDATION_EXPORT const unsigned char PGToastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGToast/PublicHeader.h>
+
+
